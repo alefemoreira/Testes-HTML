40ef723c6586dc5f34e07d68e91bb4c28a579e07
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-03-04 14:45:07 -0300

    interação html

---
+++ HTMLsnippet/javascript/script.js
@@ -0,0 +1,9 @@
+//document.write("OI");
+
+
+function changeText(){
+    var text1 = document.getElementById("principal");
+    text1.style.color = "red";
+    text1.textContent = "Olá Humano, seja bem-vindo a sua primeira interação em html";
+    alert(text1.tagName)
+}
